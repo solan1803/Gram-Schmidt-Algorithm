dacea43ac5adbd78a10abd3c6b97e688dc723678
msolan <solan1803@googlemail.com>
2016-02-13 22:03:28 +0000

    Both algorithms for gram schmidt complete

--- src/CGSAlgorithm.java
+++ src/CGSAlgorithm.java
@@ -1,55 +1,98 @@
 public class CGSAlgorithm {
 
+	static final double K = 0.1;
+	// a column vectors
+	final static double[] a1 = { 1, 0, 0, 0, K };
+	final static double[] a2 = { 1, 0, 0, K, 0 };
+	final static double[] a3 = { 1, 0, K, 0, 0 };
+	final static double[] a4 = { 1, K, 0, 0, 0 };
+	final static double[][] aMatrix = { a1, a2, a3, a4 };
+	
+	static double[][] qMatrix = { null, null, null, null };
+	static double[][] vMatrixCGS = { null, null, null, null };
+	static double[][][] vMatrixMGS = { null, null, null, null };
+	static double[][] vMatrix1 = { null, null, null, null };
+	static double[][] vMatrix2 = { null, null, null, null };
+	static double[][] vMatrix3 = { null, null, null, null };
+	static double[][] vMatrix4 = { null, null, null, null };
+
 	public static void main(String[] args) {
 
-		final double K = 0.1;
-		double[] a1 = { 1, 0, 0, 0, K };
-		double[] a2 = { 1, 0, 0, K, 0 };
-		double[] a3 = { 1, 0, K, 0, 0 };
-		double[] a4 = { 1, K, 0, 0, 0 };
-
-		double[][] aMatrix = { a1, a2, a3, a4 };
-		double[][] resultCGS = classicalGramSchmidtAlgorithm(aMatrix);
-		for (int i = 0; i < resultCGS.length; i++) {
-			for (int j = 0; j < resultCGS[0].length; j++) {
-				System.out.println(resultCGS[i][j]);
+		//classicalGramSchmidtAlgorithm();
+		modifiedGramSchmidtAlgorithm();
+		// PRINT Q MATRIX
+		for (int i = 0; i < qMatrix.length; i++) {
+			System.out.print("q" + i + ": ");
+			for (int j = 0; j < qMatrix[0].length; j++) {
+				System.out.print(qMatrix[i][j]);
+				System.out.print(", ");
 			}
 			System.out.println("");
 		}
 	}
-
-	public static double[][] classicalGramSchmidtAlgorithm(double[][] aMatrix) {
-		double[][] qMatrix = { null, null, null, null, null };
-		double[][] vMatrix = { null, null, null, null, null };
-		vMatrix[0] = aMatrix[0];
-		qMatrix[0] = scaleVector(vMatrix[0], 1 / norm(vMatrix[0]));
-
-		for (int i = 2; i < 5; i++) {
-
-			vMatrix[i - 1] = calculateV(i, aMatrix, qMatrix);
-			qMatrix[i - 1] = scaleVector(vMatrix[i - 1],
-					1 / norm(vMatrix[i - 1]));
-
-		}
-		return qMatrix;
+	
+	public static void modifiedGramSchmidtAlgorithm() {
+		
+		vMatrixMGS[0] = vMatrix1;
+		vMatrixMGS[1] = vMatrix2;
+		vMatrixMGS[2] = vMatrix3;
+		vMatrixMGS[3] = vMatrix4;
+		
+		vMatrix1[0] = a1;
+		vMatrix1[1] = a2;
+		vMatrix1[2] = a3;
+		vMatrix1[3] = a4;
+		
+		
+		
+		qMatrix[0] = scaleVector(vMatrixMGS[0][0], 1 / norm(vMatrixMGS[0][0]));
+		
+		for (int i = 1; i < aMatrix.length; i++) {
+			for (int j = i; j < aMatrix.length; j++) {
+				double scale = innerProduct(vMatrixMGS[i-1][j], qMatrix[i-1]);
+				double[] scaled = scaleVector(qMatrix[i-1], scale);
+				vMatrixMGS[i][j] = vectorOperation(vMatrixMGS[i-1][j], scaled, "-");
+			}
+			qMatrix[i] = scaleVector(vMatrixMGS[i][i], 1/norm(vMatrixMGS[i][i]));
+ 		}
+		
+		
+		
 	}
 
-	public static double[] calculateV(int index, double[][] aMatrix, double[][] qMatrix) {
+	public static void classicalGramSchmidtAlgorithm() {
+		vMatrixCGS[0] = aMatrix[0];
+		qMatrix[0] = scaleVector(vMatrixCGS[0], 1 / norm(vMatrixCGS[0]));
 
-		double[] aVector = aMatrix[index - 1];
+		for (int i = 1; i < vMatrixCGS.length; i++) {
+
+			vMatrixCGS[i] = calculateV(i);
+			qMatrix[i] = scaleVector(vMatrixCGS[i],
+					1 / norm(vMatrixCGS[i]));
+
+		}
+		
+	}
+
+	public static double[] calculateV(int index) {
+
+		double[] aVector = aMatrix[index];
 
 		double[] resultOfSummation = { 0, 0, 0, 0, 0 };
-		for (int j = 1; j <= index ; j++) {
+		for (int j = 0; j < index ; j++) {
 
-			double inner = innerProduct(aVector, qMatrix[j - 1]);
-			double[] scaled = scaleVector(qMatrix[j - 1], inner);
-			resultOfSummation = vectorOperation(scaled, qMatrix[j - 1], "+");
+			double inner = innerProduct(aVector, qMatrix[j]); // < ai, qj>
+			
+			double[] scaled = scaleVector(qMatrix[j], inner); // <ai, qj>qj
+			
+			resultOfSummation = vectorOperation(resultOfSummation, scaled, "+"); // total summation over <ai, qj>qj
 		}
+		
 
-		return vectorOperation(aVector, resultOfSummation, "-");
+		return vectorOperation(aVector, resultOfSummation, "-"); // ai - summation
 	}
 
-	public static double innerProduct(double[] a, double[] b) {
+	public static double innerProduct(final double[] a, final double[] b) {
 		assert (a.length == b.length);
 		double total = 0;
 		for (int i = 0; i < a.length; i++) {
@@ -59,33 +102,36 @@ public class CGSAlgorithm {
 
 	}
 
-	public static double[] scaleVector(double[] a, double scale) {
-
-		for (int i = 0; i < a.length; i++) {
-			a[i] *= scale;
+	public static double[] scaleVector(final double[] a, final double scale) {
+		
+		double[] copy = new double[a.length];
+		for (int i = 0; i < copy.length; i++) {
+			copy[i] = a[i] * scale;
 		}
 
-		return a;
+		return copy;
 
 	}
 
-	public static double[] vectorOperation(double[] a, double[] b, String op) {
-
+	public static double[] vectorOperation(final double[] a, final double[] b, String op) {
+		
+		assert(a.length == b.length);
+		double[] copy = new double[a.length];
 		switch (op) {
 
 		case "+":
 			for (int i = 0; i < a.length; i++) {
-				a[i] = a[i] + b[i];
+				copy[i] = a[i] + b[i];
 			}
 			break;
 		case "-":
 			for (int i = 0; i < a.length; i++) {
-				a[i] = a[i] - b[i];
+				copy[i] = a[i] - b[i];
 			}
 			break;
 		}
 
-		return a;
+		return copy;
 	}
 
 	public static double norm(double[] v) {
